37e0092c2aec877d1861d0467cf7d47d23153358
Denis Montalvan <denisitoal25@gmail.com>
2024-05-17 18:35:57 -0500

    Commit inicial

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
+// const path = require('path')
+
+// module.exports = {
+//     sassOptions: {
+//         includePaths: [path.join(__dirname, 'scss')],
+//     },
+// }
+
+// next.config.js
+// const path = require('path');
+
+// module.exports = {
+//     webpack: (config) => {
+//         config.module.rules.push({
+//             test: /\.scss$/,
+//             use: [
+//                 'style-loader',
+//                 'css-loader',
+//                 'sass-loader',
+//             ],
+//         });
+
+//         return config;
+//     },
+// };
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <header className="headline">
+            <h1 className='headline__title'>Titulo Prueba</h1>
+            <h3 className="headline__subtitle">subtitulo</h3>
+            <p className="prueba">PARRAFO</p>
+        </header>
+    )
+}
+
+export default index
